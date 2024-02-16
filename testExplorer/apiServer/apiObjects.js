d3bd4ed26dac860709632b4f5a697f0084277dea
Maidzolaola <maidzola@hijra.id>
2024-02-16 18:00:33 +0700

    commit kelima

--- testExplorer/apiServer/apiObjects.js
+++ testExplorer/apiServer/apiObjects.js
@@ -3,13 +3,7 @@ const { expect } = require("chai"); // import chai
 const baseUrl = require("../globalVariable/baseUrl");
 
 const url = baseUrl; //define global variable
-
-async function getMethod() {
-  const response = await request(url).get("/objects/12");
-  //assertation / verifikasi
-  expect(response.status).to.equal(200);
-  console.log(response.body);
-}
+  
 
 async function postMethod() {
   const response = await request(url)
@@ -25,8 +19,49 @@ async function postMethod() {
   //assertation / verifikasi
   expect(response.status).to.equal(200);
   expect(response.body.name).to.equal("Agus Budiman");
-
-  console.log(response.body);
+//   expect(response.data.Generation).to.equal("2024th");
+//   expect(response.data.Price).to.equal("10000");
+//   expect(response.data.Capacity).to.equal("125 GB");
+  expect(response.body.createdAt).to.not.be.null;
+  //simpan id after POST
+  const id = response.body.id;
+  console.log("id after POST:", id);
+  return id; //Return the id
 }
 
-module.exports = { getMethod, postMethod };
+
+async function getMethod(id) {
+    const response = await request(url).
+    get(`/objects/${id}`)
+    //assertation / verifikasi
+    expect(response.status).to.equal(200);
+    expect(response.body.id).to.equal(id);
+    expect(response.body.name).to.equal("Agus Budiman");
+    // expect(response.data.Generation).to.equal("2024th");
+    // expect(response.data.Price).to.equal("10000");
+    // expect(response.data.Capacity).to.equal("125 GB");
+    console.log(response.body);
+  }
+  
+
+async function putMethod(id) {
+    const response = await request(url)
+      .put(`/objects/${id}`)
+      .send({
+        name: "Agus Budiman PUT",
+        data: {
+        Generation: "2024th",
+        Price: "10000",
+        Capacity: "125 GB",
+        // Guarantee: "5 year",
+        },
+      });
+    //assertation / verifikasi
+    expect(response.status).to.equal(200);
+    // expect(response.data.Guarantee).to.equal("5 year");
+    expect(response.body.name).to.equal("Agus Budiman PUT");
+  }
+
+
+
+module.exports = { postMethod, getMethod, putMethod };
--- testExplorer/testCase/testCaseFunction_02.js
+++ testExplorer/testCase/testCaseFunction_02.js
@@ -1,12 +1,15 @@
-const { getMethod, postMethod } = require("../apiServer/apiObjects");
+const { postMethod, getMethod, putMethod } = require("../apiServer/apiObjects");
 
 //test suites
-describe("Testing API restful", function () {
+describe("Testing API restful 2", function () {
   // Test case 1
+  it("Test POST from Function", async function () {
+    await postMethod();
+  });
   it("Test GET from Function", async function () {
     await getMethod();
   });
-  it("Test POST from Function", async function () {
-    await postMethod();
+  it("Test PUT from Function", async function () {
+    await putMethod();
   });
 });
